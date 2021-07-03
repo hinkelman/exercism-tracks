a2de4f1aec8456f990714a1f5242f7a53250f5d3
Travis Hinkelman <travis.hinkelman@gmail.com>
2021-07-03 14:43:32 -0700

    Add clojure rle

---
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,43 @@
+(ns run-length-encoding)
+
+(defn rle-same [xs]
+  (let [n (count xs)
+        N (if (= n 1) "" n)]
+    (str N (first xs))))
+ 
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [plain-text]
+  (apply str (map rle-same (partition-by identity plain-text))))
+
+(defn digit? [c] (and (>= (compare c \0) 0) 
+                      (<= (compare c \9) 0)))
+
+(defn convert-icl [icl letter]
+  (let [n (Integer/parseInt (apply str icl))]
+    (apply str (repeat n letter))))
+
+(defn rld-helper [xs icl acc] ; icl = int char list
+  (if (empty? xs)
+    (apply str (reverse acc))
+    (let [x (first xs)]
+      (if (digit? x)
+        (recur (rest xs) (cons x icl) acc)
+        (if (empty? icl)
+          (recur (rest xs) icl (cons (str x) acc))
+          (recur (rest xs) '() (cons (convert-icl icl x) acc)))))))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text]
+  (loop [xs (char-array cipher-text)
+         icl '()
+         acc '()]
+    (if (empty? xs)
+      (apply str (reverse acc))
+      (let [x (first xs)]
+        (if (digit? x)
+          (recur (rest xs) (cons x icl) acc)
+          (if (empty? icl)
+            (recur (rest xs) icl (cons (str x) acc))
+            (recur (rest xs) '() (cons (convert-icl icl x) acc))))))))
